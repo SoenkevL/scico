6c12ef545f390cce7ccb651f9875eb316a823a71
Soenke van Loh <soenke.v.l@web.de>
2025-09-21 23:02:32 +0200

    update `configs.py`: change default `ollama_model` to `gpt-oss:latest` and relocate `headers_to_split_on` definition

--- src/configs.py
+++ src/configs.py
@@ -3,9 +3,20 @@ extractor_ollama_config = {
     'use_llm': True,
     'llm_service': 'marker.services.ollama.OllamaService',
     'ollama_base_url': 'http://localhost:11434',
-    'ollama_model': 'qwen2.5:14b'
+    'ollama_model': 'gpt-oss:latest'
 }
 
+headers_to_split_on = [
+            ("#", "level1"),
+            ("##", "level2"),
+            ("###", "level3"),
+            ("####", "level4"),
+            ("#####", "level5"),
+            ("######", "level6"),
+            ("#######", "level7")
+]
+
+# currently not in use
 embedding_config = {
     "autoid": "uuid5",
     "path": "intfloat/e5-base",
@@ -19,13 +30,3 @@ embedding_config = {
         "topics": {}
     }
 }
-
-headers_to_split_on = [
-            ("#", "level1"),
-            ("##", "level2"),
-            ("###", "level3"),
-            ("####", "level4"),
-            ("#####", "level5"),
-            ("######", "level6"),
-            ("#######", "level7")
-]
