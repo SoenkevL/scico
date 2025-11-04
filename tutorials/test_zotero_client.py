229e3fe596f17050593792ea0c1b5ede1bfb39cf
Soenke van Loh <soenke.v.l@web.de>
2025-11-04 17:11:27 +0100

    add tutorials: introduce `scico_example_agent_workflow.py` for demonstrating the AI agent workflow, `test_zotero_client.py` for testing Zotero client capabilities, and update related Jupyter notebooks with query functionality and consistent imports

---
+++ tutorials/test_zotero_client.py
@@ -0,0 +1,34 @@
+# %% initialize
+from pprint import pprint
+
+from src.zotero_client import ZoteroClient
+
+client = ZoteroClient()
+
+
+# %%
+def seperator():
+    print('=' * 100)
+
+
+# %% basic info
+print(client.get_item_count())
+
+pprint(client.list_all_collections())
+
+# %% search functionallity
+pprint(client.get_items_by_collection_id("K7QSQYKD"))
+seperator()
+
+# %%
+item = client.get_items_by_id("VK5V8ET7")
+pprint(item)
+
+# %%
+pprint(client.get_items_by_collection_name("EEG_For_Meditation"))
+
+# %%
+pprint(client.get_items_by_id("YDI66MQF"))
+
+# %%
+pprint(client.get_items_by_name("EEG"))
